dbd57557706c1a362e62339898028111e56d2256
Wesley Ferreira <wesleyfsilva97@gmail.com>
2026-05-16 02:50:33 -0300

    style: formata código com Prettier para melhor legibilidade

--- src/routes.ts
+++ src/routes.ts
@@ -1,18 +1,23 @@
-import { Router } from 'express';
-import { register, login } from './controllers/authController';
-import { getAllArticles, createArticle, updateArticle, deleteArticle } from './controllers/articleController';
-import { authMiddleware } from './middlewares/auth';
+import { Router } from "express";
+import { register, login } from "./controllers/authController";
+import {
+  getAllArticles,
+  createArticle,
+  updateArticle,
+  deleteArticle,
+} from "./controllers/articleController";
+import { authMiddleware } from "./middlewares/auth";
 
 const router = Router();
 
 // Rotas Públicas
-router.post('/register', register);
-router.post('/login', login);
-router.get('/articles', getAllArticles);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/articles", getAllArticles);
 
 // Rotas Protegidas (precisam de login)
-router.post('/articles', authMiddleware, createArticle);
-router.put('/articles/:id', authMiddleware, updateArticle);
-router.delete('/articles/:id', authMiddleware, deleteArticle);
+router.post("/articles", authMiddleware, createArticle);
+router.put("/articles/:id", authMiddleware, updateArticle);
+router.delete("/articles/:id", authMiddleware, deleteArticle);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,12 @@
-import express from 'express';
-import cors from 'cors';
-import router from './routes';
+import express from "express";
+import cors from "cors";
+import router from "./routes";
 
 const app = express();
 
 app.use(cors());
 // Permite receber payloads maiores por conta da conversão das imagens em string
-app.use(express.json({ limit: '10mb' })); 
+app.use(express.json({ limit: "10mb" }));
 app.use(router);
 
 const PORT = process.env.PORT || 3001;
